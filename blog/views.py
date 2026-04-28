aee571a40b6519e64d220c5844c1822d93af8a37
유재현 <yslylove1234@gmail.com>
2026-04-28 11:17:42 +0900

    CBV로 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     # template_name = 'blog/index.html'
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
 # FBV 방법
@@ -21,6 +24,8 @@ class PostList(ListView):
 #     )
 
 # def single_post_page(request, pk):
+
+# pk=pk -> 데이터베이스 안에 키를 딱 하나 찍어서 가져온다 는 것
 #     post = Post.objects.get(pk=pk)
 
 #     return render(
