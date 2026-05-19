d64525b0b1be2b9f01f2597f4e428121f39932a4
유재현 <yslylove1234@gmail.com>
2026-05-19 10:25:29 +0900

    파일 업로드 및 이미지 나타내기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     # True 옵션 시 데이터가 처음 생성될 때 현재 시간 저장
     updated_at = models.DateTimeField(auto_now=True)
--- jh_django_prj/urls.py
+++ jh_django_prj/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('blog/', include('blog.urls')), 
     # include는 blog.urls 파일을 참고해서 화면에 보여지는 것을 다운로드 함
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
